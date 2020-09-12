31db4f8e0f7f0362122a05be3a6e5c1fffebaff6
Eduardo Copat <ecopat@gmail.com>
2020-09-13 10:10:35 +1200

    mkdocs.yml adjustments

--- download.sh
+++ download.sh
@@ -4,3 +4,5 @@
 mkdir -p sapdocs
 
 httrack "https://help.sap.com/doc/abapdocu_731_index_htm/7.31/en-US/abenabap.htm" -O "./sapdocs" "+*.help.sap.com/doc/*" -v
+httrack "https://help.sap.com/doc/abapdocu_740_index_htm/7.40/en-US/abenabap.htm" -O "./sapdocs" "+*.help.sap.com/doc/*" -v
+httrack "https://help.sap.com/doc/abapdocu_754_index_htm/7.54/en-US/abenabap.htm" -O "./sapdocs" "+*.help.sap.com/doc/*" -v
--- index.ts
+++ index.ts
@@ -16,28 +16,34 @@ let DEBUG = false;
 
 if (args[0] === '--debug') { DEBUG = true; }
 // For debugging purposes, jsut change this to true
-
-files.forEach((file) => {
-  if (DEBUG) {
-    if (
-      file.name !== 'abapread_table_key'
-     && file.name !== 'abapmethods_general'
-     && file.name !== 'abapappend'
-     && file.name !== 'abapdata_options'
-     && file.name !== 'abapread_table') {
-      return;
+try {
+  files.forEach((file) => {
+    if (DEBUG) {
+      if (
+        file.name !== 'abapread_table_key'
+        && file.name !== 'abapmethods_general'
+        && file.name !== 'abapappend'
+        && file.name !== 'abapdata_options'
+        && file.name !== 'abapread_table') {
+        return;
+      }
     }
-  }
 
-  process.stdout.write(`processing ${chalk.blue(file.path)} \n`);
-  const parser = new Parser(file.cheerio, new Renderer());
-  const contents = parser.parse();
+    process.stdout.write(`processing ${chalk.blue(file.path)} \n`);
+    const parser = new Parser(file.cheerio, new Renderer());
+    const contents = parser.parse();
 
-  fse.outputFile(path.join(__dirname, `./docs/7.4/${file.name}.md`), contents, (err: any) => {
-    if (!err) return;
-    process.stderr.write(chalk.red(err));
-    process.exitCode = 1;
+    fse.outputFile(path.join(__dirname, `./docs/7.4/${file.name}.md`), contents, (err: any) => {
+      if (!err) return;
+      process.stderr.write(chalk.red(err));
+      process.exitCode = 1;
+    });
   });
-});
 
-process.stdout.write(chalk.green('Success\n'));
+  fse.copySync('base-mkdocs.yml', 'mkdocs.yml');
+  process.stdout.write(chalk.green('Copied mkdocs.yml\n'));
+
+  process.stdout.write(chalk.green('Success\n'));
+} catch (err) {
+  process.stderr.write(chalk.red(err));
+}
